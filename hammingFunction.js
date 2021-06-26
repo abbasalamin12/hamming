81a2f8c18d04eea47d1c25e4a32196ad6fc92518
abbasalamin12 <1357468+abbasalamin12@users.noreply.github.com>
2021-06-26 17:50:31 +0100

    first commit

---
+++ hammingFunction.js
@@ -0,0 +1,20 @@
+const compute = (stringA, stringB) => {
+  if (stringA.length == 0 && stringB.length == 0) return 0;
+
+  // error handling
+  if (stringA.length == 0) throw new Error("left strand must not be empty");
+  if (stringB.length == 0) throw new Error("right strand must not be empty");
+  if (stringA.length != stringB.length) {
+    throw new Error("left and right strands must be of equal length");
+  }
+
+  // loop through each character in the string and check for equality
+  const strLength = stringA.length;
+  let hammingDistance = 0;
+
+  for (let i = 0; i < strLength; i++) {
+    if (stringA.substr(i, 1) != stringB.substr(i, 1)) hammingDistance++;
+  }
+
+  return hammingDistance;
+};
